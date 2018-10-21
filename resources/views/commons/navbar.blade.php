8d8aeda2496fc05f7cd1c434d396c9a76031332c
EC2 Default User <ec2-user@ip-172-31-86-190.ec2.internal>
2018-10-21 11:56:15 +0000

    top page

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,27 @@
+<header>
+    <nav class="navbar navbar-default navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
+                
+                <!-- 横幅が狭い時に出るハンバーガーボタン -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                
+                <!-- ホームへ戻るリンク。ブランドロゴなどを置く。  public/ 直下に public/images/logo.png を-->
+                <a class="navbar-left" href="/"><img src="{{ secure_asset("images/logo.png") }}" alt="Monolist"></a>
+            </div>
+            
+            <!-- メニュー項目 -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#">新規登録</a></li>
+                    <li><a href="#">ログイン</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
